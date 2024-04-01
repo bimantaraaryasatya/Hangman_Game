ada12d91b1abaed0b53770af00b25c65b19da150
bimantaraaryasatya <bimantara_aryasatya_32rpl@student.smktelkom-mlg.sch.id>
2024-04-01 13:12:44 +0700

    first commit

---
+++ word-list.js
@@ -0,0 +1,42 @@
+const wordList = [
+    {
+      word: "guitar",
+      hint: "A populer musical instrument with strings"
+    },
+    {
+      word: "oxygen",
+      hint: "A colorless, odorless gas essential for life"
+    },
+    {
+      word: "mountain",
+      hint: "A large natural elevation of the Earth's surface"
+    },
+    {
+      word: "keyboard",
+      hint: "A tool for typing on the computer"
+    },
+    {
+      word: "javaScript",
+      hint: "A populer programming language"
+    },
+    {
+      word: "jakarta",
+      hint: "The capital city of Indonesia"
+    },
+    {
+      word: "surabaya",
+      hint: "The capital city of East Java Indonesia"
+    },
+    {
+      word: "jupiter",
+      hint: "The bigest planet in the solar system"
+    },
+    {
+      word: "watch",
+      hint: "A tool to see the time"
+    },
+    {
+      word: "computer",
+      hint: "Main tool for programming"
+    },
+  ];
